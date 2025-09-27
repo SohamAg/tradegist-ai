679a8db95f75d2f7de87dc33caf6604f5ee89e9c
SohamAg <108403034+SohamAg@users.noreply.github.com>
2025-09-27 15:09:24 -0400

    Delete web directory

--- web/src/lib/supabase-server.ts
+++
@@ -1,5 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { Database } from './database.types'
-
-export const createClient = () => createServerComponentClient<Database>({ cookies })
